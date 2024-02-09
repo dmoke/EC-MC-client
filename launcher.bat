691ca7c069f40ed544d1489b90b897686d9894c6
dmoke <dmytromykhaliukk@gmail.com>
2024-02-09 14:24:02 +0200

    add: opti mods; physics

--- launcher.bat
+++ launcher.bat
@@ -1,4 +1,3 @@
-@echo off
-cd %~dp0
-start javaw -jar launcher.jar
-exit
+version https://git-lfs.github.com/spec/v1
+oid sha256:994b9f395bd9de63d5066853b8baf48ff7a8cedf9462d5d37821c17e8ffc8ba3
+size 54
